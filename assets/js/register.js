2c892eeee52ee03686db4d059a1f5be430005000
yovikoesniawan <92287513+yovikoesniawan@users.noreply.github.com>
2022-11-15 14:53:26 +0700

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", init, false)
 function init() {
     if ("serviceWorker" in navigator && navigator.onLine) {
-        navigator.serviceWorker.register("/service-worker.js").then(
+        navigator.serviceWorker.register("/cafe-sederhana/service-worker.js").then(
             (reg) => {
                 console.log("Registrasi service worker Berhasil", reg)
             },
